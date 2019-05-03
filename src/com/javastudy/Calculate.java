afe9a2f7c2d88e2f13a6baa6055ae3e622826b0f
xiaobei.wu <303715909@qq.com>
2019-05-03 21:57:49 +0800

    single test add

---
+++ src/com/javastudy/Calculate.java
@@ -0,0 +1,27 @@
+package com.javastudy;
+
+public class Calculate {
+	
+	
+	
+	public int add(int x,int y) {
+		return x+y;
+			
+	}
+	
+	
+	public int sub(int x,int y) {
+		return x-y;
+			
+	}
+	
+	public int mult(int x,int y) {
+		return x*y;
+			
+	}
+	
+	public int div(int x,int y) {
+		return x/y;
+			
+	}
+}
